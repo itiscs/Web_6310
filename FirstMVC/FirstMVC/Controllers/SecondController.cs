c35e3a82f5c17fe2b35e0ba993db97d25d9d3da5
itiscs <moudroff@gmail.com>
2025-10-10 10:32:20 +0300

    Init

---
+++ FirstMVC/FirstMVC/Controllers/SecondController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Card()
+        {
+            return View();
+        }
+    }
+}
