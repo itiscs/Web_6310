2368b5119af94d5fb25e0832448291a7b52f6322
itiscs <moudroff@gmail.com>
2025-10-17 17:19:53 +0300

    Add Model

---
+++ FirstMVC/FirstMVC/Data/MoviesContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using FirstMVC.Models;
+
+namespace FirstMVC.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext (DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; } = default!;
+        
+
+    }
+}
---
+++ FirstMVC/FirstMVC/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstMVC.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Display(Name = "Название")]
+        public string? Title { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Дата релиза")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Жанр")]
+        public string? Genre { get; set; }
+
+        [Display(Name = "Цена")]
+        public decimal Price { get; set; }
+    }
+}
