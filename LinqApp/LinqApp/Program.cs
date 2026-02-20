de5458cc0775a4081f2f960b661e441a224d4058
itiscs <moudroff@gmail.com>
2026-02-20 10:55:15 +0300

    Linq GroupBy

---
+++ LinqApp/LinqApp/Program.cs
@@ -0,0 +1,66 @@
+﻿
+using LinqApp;
+
+var mas = new int[]{ 1, 2, 3, 6, -7, 2, -4, 3, 15, -8, 2, 6 };
+
+
+var m1 = mas.Where(x => x > 0).Skip(3).OrderBy(x => x % 3).Any(x=>x>10);
+
+Console.WriteLine(m1);
+
+//foreach (var x in m1)
+//    Console.WriteLine(x);
+Console.WriteLine("**************************************");
+
+var l = Student.GetStudents().Where(s => s.Age == 20).SelectMany(s => s.Marks);
+
+foreach (var m in l)
+{
+    Console.WriteLine(m);
+}
+
+
+
+Console.WriteLine("**************************************");
+
+var lst = Student.GetStudents().Where(s => s.Age > 20)
+    .OrderByDescending(s => s.Marks.Average())
+    .Select(s => new { s.Name, s.Age, Coef = s.Age*s.Id })
+    .OrderBy(a=>a.Coef);
+
+foreach (var s in lst)
+{
+    Console.WriteLine(s);
+}
+
+
+Console.WriteLine("**************************************");
+
+var gr = Student.GetStudents().GroupBy(s => s.Age)
+    .Select(g => new { Age = g.Key, Count = g.Count(), Max = g.Min(s => s.Name) });
+
+foreach (var g in gr)
+{
+    Console.WriteLine(g);
+
+}
+
+var gr2 = Student.GetStudents().GroupBy(s => new { s.Age, s.Marks.Count })
+    .Select(g => new {g.Key, Count = g.Count(), Max = g.Min(s => s.Name) });
+
+foreach (var g in gr2)
+{
+    Console.WriteLine(g);
+}
+
+
+Console.WriteLine("**************************************");
+
+//var st = Student.GetStudents().OrderByDescending(s => s.Name[s.Name.Length - 1]).First();
+
+var st = Student.GetStudents().LastOrDefault(s=>s.Age==28);
+
+if (st != null)
+    Console.WriteLine(st);
+else
+    Console.WriteLine("нет таких");
---
+++ LinqApp/LinqApp/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqApp
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  int Age { get; set; }
+        public List<int> Marks { get; set; } = new List<int>();
+
+        public override string ToString()
+        {
+            return $"{Name} {Age} {Id} {Marks.Average()}";
+        }
+
+        public static List<Student> GetStudents()
+        {
+            var lst = new List<Student>
+            {
+                new Student { Id = 1,  Name = "Алексей",     Age = 20, Marks = new List<int> { 85, 90, 78, 92, 88 } },
+                new Student { Id = 2,  Name = "Борис",       Age = 22, Marks = new List<int> { 75, 80, 68, 72, 70 } },
+                new Student { Id = 3,  Name = "Виталий",     Age = 19, Marks = new List<int> { 95, 88, 91, 90, 93 } },
+                new Student { Id = 4,  Name = "Галина",      Age = 21, Marks = new List<int> { 82, 84,  85, 81 } },
+                new Student { Id = 5,  Name = "Дарья",       Age = 18, Marks = new List<int> { 90, 92, 89, 94, 91 } },
+                new Student { Id = 6,  Name = "Егор",        Age = 23, Marks = new List<int> { 68, 72, 70, 65, 74 } },
+                new Student { Id = 7,  Name = "Жанна",       Age = 20, Marks = new List<int> { 88, 86, 90, 87, 85 } },
+                new Student { Id = 8,  Name = "Иван",        Age = 19, Marks = new List<int> { 77, 79, 75, 80, 78 } },
+                new Student { Id = 9,  Name = "Константин",  Age = 24, Marks = new List<int> { 83, 85, 81, 84, 82 } },
+                new Student { Id = 10, Name = "Лариса",      Age = 21, Marks = new List<int> { 91, 89, 92, 90, 88 } },
+                new Student { Id = 11, Name = "Мария",       Age = 18, Marks = new List<int> { 95, 94, 96, 93, 97,51 } },
+                new Student { Id = 12, Name = "Никита",      Age = 22, Marks = new List<int> { 70, 72, 68, 73, 71 } },
+                new Student { Id = 13, Name = "Олег",        Age = 20, Marks = new List<int> { 80, 82, 78, 79, 81 } },
+                new Student { Id = 14, Name = "Полина",      Age = 19, Marks = new List<int> { 86, 88, 85, 87, 89 } },
+                new Student { Id = 15, Name = "Роман",       Age = 23, Marks = new List<int> { 74, 76, 72, 75, 73,65 } },
+                new Student { Id = 16, Name = "Светлана",    Age = 21, Marks = new List<int> { 92, 90, 91, 89, 93 } },
+                new Student { Id = 17, Name = "Тимур",       Age = 18, Marks = new List<int> { 66, 68, 64, 67, 65 } },
+                new Student { Id = 18, Name = "Ульяна",      Age = 20, Marks = new List<int> { 88, 90, 87, 89, 91 } },
+                new Student { Id = 19, Name = "Федор",       Age = 24, Marks = new List<int> { 78, 80, 76, 79, 77 } },
+                new Student { Id = 20, Name = "Юлия",        Age = 22, Marks = new List<int> { 93, 95, 92, 94, 96 } }
+            };
+
+
+            return lst;
+        } 
+
+
+
+    }
+}
