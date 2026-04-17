df1dbea80defdd5e7b65fbc4d05d7c2fd6f05a4f
itiscs <moudroff@gmail.com>
2026-04-17 10:45:11 +0300

    Web Api First

---
+++ WebApiFirst/WebApiFirst/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiFirst.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
